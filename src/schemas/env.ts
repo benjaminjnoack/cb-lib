6b1b0f1f639149be9c2c38df609bd484706e96bc
Benjamin Noack <benjamin.noack@icloud.com>
2026-02-16 08:43:21 -0500

    extend env to include postgres credentials

--- src/schemas/env.ts
+++ src/schemas/env.ts
@@ -2,7 +2,10 @@ import { z } from "zod";
 
 export const EnvSchema = z
   .object({
-    HELPER_COINBASE_CREDENTIALS_PATH: z.string().min(1)
+    HELPER_COINBASE_CREDENTIALS_PATH: z.string().min(1),
+    HELPER_POSTGRES_DATABASE: z.string().min(1).optional(),
+    HELPER_POSTGRES_USERNAME: z.string().min(1).optional(),
+    HELPER_POSTGRES_PASSWORD: z.string().min(1).optional(),
   }).loose();
 
 export type Env = z.infer<typeof EnvSchema>;
